7d048355747c02afaa8b9929e20f03c453cb2a55
hls <hilliassmirali@gmail.com>
2025-04-11 14:19:20 +0200

    fix: ensure newline at end of file in multiple files; add __init__.py and all_repo.py

--- part4/app/services/facade.py
+++ part4/app/services/facade.py
@@ -1,81 +1,96 @@
+from app.persistence.all_repo import UserRepository, PlaceRepository, AmenitiesRepository, ReviewRepository
+from app.models.place import Place
 from app.models.user import User
 from app.models.review import Review
-from app.models.place import Place
 from app.models.amenity import Amenity
-from app.persistence.repository import UserRepository, PlaceRepository
-from app.persistence.repository import ReviewRepository, AmenityRepository
+
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = UserRepository()
-        self.review_repo = ReviewRepository()
-        self.amenity_repo = AmenityRepository()
         self.place_repo = PlaceRepository()
+        self.review_repo = ReviewRepository()
+        self.amenity_repo = AmenitiesRepository()
 
-    # User methods
+
+    """User facade"""
     def create_user(self, user_data):
-        password = user_data.pop('password')
         user = User(**user_data)
-        user.hash_password(password)
+        user.hash_password(user_data['password'])
         self.user_repo.add(user)
         return user
 
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
-
-    def get_user_by_email(self, email):
-        return self.user_repo.get_user_by_email(email)
-
+    
     def get_all_users(self):
-        return self.user_repo.get_all()
+         return self.user_repo.get_all()
+    
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     def update_user(self, user_id, user_data):
         self.user_repo.update(user_id, user_data)
         return self.user_repo.get(user_id)
-
-    def delete_user(self, user_id):
-        user = self.user_repo.get(user_id)
-        if not user:
-            raise ValueError(f"User with ID {user_id} not found")
-        self.user_repo.delete(user)
-        return user
-
-    # Place Methods
-    def create_place(self, place_data):
+    
+    """Place facade"""
+    def create_place(self, place_data, amenities):
         place = Place(**place_data)
-        self.place_repo.add(place)
+        self.place_repo.add(place, amenities)
         return place
 
     def get_place(self, place_id):
         return self.place_repo.get(place_id)
-    
+
     def get_all_places(self):
         return self.place_repo.get_all()
 
-    def update_place(self, place_id, place_data):
-        return self.place_repo.update(place_id, place_data)
-
+    def update_place(self, place_id, place_data, amenities):
+        self.place_repo.update(place_id, place_data, amenities)
+        return self.place_repo.get(place_id)
+    
     def delete_place(self, place_id):
-        return self.place_repo.delete(place_id)
+        self.place_repo.delete(place_id)
+        return self.place_repo.get_all()
 
-    # Review Methods
+    """Review facade"""
     def create_review(self, review_data):
         review = Review(**review_data)
-        return self.review_repo.add(review)
+        self.review_repo.add(review)
+        return review
 
     def get_review(self, review_id):
         return self.review_repo.get(review_id)
 
-    def delete_review(self, review_id):
-        return self.review_repo.delete(review_id)
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
 
-    # Amenity Methods
+    def get_reviews_by_place(self, place_id):
+        return self.review_repo.get_all_by_attribute('place_id', place_id)
+
+    def update_review(self, review_id, review_data):
+        self.review_repo.update(review_id, review_data)
+        return self.review_repo.get(review_id)
+
+    def delete_review(self, review_id):
+        self.review_repo.delete(review_id)
+        return self.review_repo.get_all()
+
+    """Amenity Facades"""
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
-        return self.amenity_repo.add(amenity)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity_by_name(self, name):
+        return self.amenity_repo.get_by_attribute('name', name)
 
     def get_amenity(self, amenity_id):
         return self.amenity_repo.get(amenity_id)
 
-    def delete_amenity(self, amenity_id):
-        return self.amenity_repo.delete(amenity_id)
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.amenity_repo.get(amenity_id)
